af571c89f15a24f5731e6e1d6f0fc19a99857c40
SriVivekanandan <70656799+SriVivekanandan@users.noreply.github.com>
2020-11-17 15:38:29 -0600

    NameTage Corrections
    Used substring and indexOf instead of using split and arrays to display the chunks of the name

--- chapter 1&2/nameTag.java
+++ chapter 1&2/nameTag.java
@@ -10,19 +10,30 @@ public class nameTag
     public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     System.out.println("Please enter your name: ");
+    
     String name = input.nextLine();
-    String split_name[] = name.split(" ");
-    System.out.println(split_name[0] + " " + split_name[1]);
-    System.out.println(split_name[2]);
+    int spaces = name.indexOf(" " , name.indexOf(" ") +1); //find index of second space in string
+    //System.out.println("Index: " + spaces);  in code test if unsure what spaces is being set as
+    String first_middle = name.substring(0,spaces); //get first and middle name chunk
+   
+    String last = name.substring(spaces + 1); //last name chunk
+    
+    
+    System.out.println(first_middle);
+    System.out.println(last);
+    
+    //String split_name[] = name.split(" ");
+    //System.out.println(split_name[0] + " " + split_name[1]);
+    //System.out.println(split_name[2]);
 
 
   }
     
 
-    // Please enter your name: 
-    // Sriram C. Vivekanandan
-    // Sriram C.
-    // Vivekanandan
+    //Please enter your name: 
+    //Sriram C. Vivekanandan
+    //Sriram C.
+    //Vivekanandan
     
     
     
